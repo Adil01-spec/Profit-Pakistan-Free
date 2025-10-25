56a01e8406d6e98964c068ebd3f17bd37cc4d0a3
Adil Zia <RajaAdil148@gmail.com>
2025-10-25 07:40:21 +0000

    Make this disappear (_for element <Button>_)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,6 @@ import { Logo } from '@/components/logo';
 import { ThemeToggle } from '@/components/theme-toggle';
 import { Button } from './ui/button';
 import { FolderClock } from 'lucide-react';
-import { SettingsDialog } from './settings-dialog';
 import { useRouter } from 'next/navigation';
 
 export function Header() {
@@ -30,7 +29,6 @@ export function Header() {
                 <span className="sr-only">My Reports</span>
             </Link>
           </Button>
-          <SettingsDialog />
           <ThemeToggle />
           <Button
             className="rounded-full text-sm font-medium px-5 py-2 transition-colors bg-primary hover:bg-primary/90"
