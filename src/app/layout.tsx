1123f268dfeb49b473e062e174a874daf678fb62
Adil Zia <RajaAdil148@gmail.com>
2025-11-01 15:28:27 +0000

    Try fixing this error: `Runtime ChunkLoadError: Loading chunk app/layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import { SettingsProvider } from '@/hooks/use-settings';
 import { HistoryProvider } from '@/hooks/use-history';
 import { Header } from '@/components/header';
 import { Footer } from '@/components/footer';
-import Script from 'next/script';
 import type { Metadata } from 'next';
 import { ReactNode } from 'react';
 import { FirebaseProvider } from '@/firebase/provider';
@@ -43,13 +42,6 @@ export default function RootLayout({ children }: Readonly<{children: React.React
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
-        <Script
-          id="adsbygoogle-script"
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-XXXXXXXXXXXXXX"
-          crossOrigin="anonymous"
-          strategy="lazyOnload"
-        />
       </head>
       <body className={cn('font-body antialiased flex flex-col min-h-screen')}>
         <FirebaseProvider>
